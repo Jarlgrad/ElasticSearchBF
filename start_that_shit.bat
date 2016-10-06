64a53ae445c22bdc52c1de89373deae573b7f045
Timothy Boris Jalgard <timothy.jalgard@gmail.com>
2016-10-06 09:56:27 +0200

    Ändrat scriptet för att köra npm start korrekt.

--- start_that_shit.bat
+++ start_that_shit.bat
@@ -1,5 +1,4 @@
 start C:\Users\Administrator\Dev\elasticsearch-2.4.0\bin\elasticsearch.bat
 cd C:\Users\Administrator\Source\Repos\ElasticSearchBF\
 code Frontend
-cd C:\Users\Administrator\Source\Repos\ElasticSearchBF\Frontend
-start npm start
+start npm start Frontend
